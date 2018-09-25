724144b7692c1dbf8b9867de5f003e6e2e924940
Kensaku Araga <ken39arg@gmail.com>
2018-09-25 16:29:25 +0900

    validationを加えておく

--- bench/isulog/isulog.go
+++ bench/isulog/isulog.go
@@ -49,8 +49,28 @@ type Signup struct {
 	UserID int64  `json:"user_id"`
 }
 
+func (d *Signup) Validate() error {
+	if d.Name == "" {
+		return errors.Errorf("name is empty.")
+	}
+	if d.BankID == "" {
+		return errors.Errorf("bank_id is empty.")
+	}
+	if d.UserID < 1 {
+		return errors.Errorf("user_id is must be upper than 1.")
+	}
+	return nil
+}
+
 type Signin struct {
 	UserID int64 `json:"user_id"`
+}
+
+func (d *Signin) Validate() error {
+	if d.UserID < 1 {
+		return errors.Errorf("user_id is must be upper than 1.")
+	}
+	return nil
 }
 
 type Order struct {
@@ -60,6 +80,22 @@ type Order struct {
 	Price   int64 `json:"price"`
 }
 
+func (d *Order) Validate() error {
+	if d.UserID < 1 {
+		return errors.Errorf("user_id is must be upper than 1.")
+	}
+	if d.OrderID < 1 {
+		return errors.Errorf("order_id is must be upper than 1.")
+	}
+	if d.Amount < 1 {
+		return errors.Errorf("amount is must be upper than 1.")
+	}
+	if d.Price < 1 {
+		return errors.Errorf("price is must be upper than 1.")
+	}
+	return nil
+}
+
 type BuyError struct {
 	UserID int64  `json:"user_id"`
 	Amount int64  `json:"amount"`
@@ -67,10 +103,39 @@ type BuyError struct {
 	Error  string `json:"error"`
 }
 
+func (d *BuyError) Validate() error {
+	if d.UserID < 1 {
+		return errors.Errorf("user_id is must be upper than 1.")
+	}
+	if d.Amount < 1 {
+		return errors.Errorf("amount is must be upper than 1.")
+	}
+	if d.Price < 1 {
+		return errors.Errorf("price is must be upper than 1.")
+	}
+	if d.Error == "" {
+		return errors.Errorf("error is empty.")
+	}
+	return nil
+}
+
 type Trade struct {
 	TradeID int64 `json:"trade_id"`
 	Amount  int64 `json:"amount"`
 	Price   int64 `json:"price"`
+}
+
+func (d *Trade) Validate() error {
+	if d.TradeID < 1 {
+		return errors.Errorf("trade_id is must be upper than 1.")
+	}
+	if d.Amount < 1 {
+		return errors.Errorf("amount is must be upper than 1.")
+	}
+	if d.Price < 1 {
+		return errors.Errorf("price is must be upper than 1.")
+	}
+	return nil
 }
 
 type OrderTrade struct {
@@ -81,10 +146,42 @@ type OrderTrade struct {
 	Price   int64 `json:"price"`
 }
 
+func (d *OrderTrade) Validate() error {
+	if d.TradeID < 1 {
+		return errors.Errorf("trade_id is must be upper than 1.")
+	}
+	if d.UserID < 1 {
+		return errors.Errorf("user_id is must be upper than 1.")
+	}
+	if d.OrderID < 1 {
+		return errors.Errorf("order_id is must be upper than 1.")
+	}
+	if d.Amount < 1 {
+		return errors.Errorf("amount is must be upper than 1.")
+	}
+	if d.Price < 1 {
+		return errors.Errorf("price is must be upper than 1.")
+	}
+	return nil
+}
+
 type OrderDelete struct {
 	OrderID int64  `json:"order_id"`
 	UserID  int64  `json:"user_id"`
 	Reason  string `json:"reason"`
+}
+
+func (d *OrderDelete) Validate() error {
+	if d.UserID < 1 {
+		return errors.Errorf("user_id is must be upper than 1.")
+	}
+	if d.OrderID < 1 {
+		return errors.Errorf("order_id is must be upper than 1.")
+	}
+	if d.Reason != "canceled" && d.Reason != "reserve_failed" {
+		return errors.Errorf("reason is must be canceled or reserve_failed.")
+	}
+	return nil
 }
 
 type Isulog struct {
@@ -110,7 +207,7 @@ func (b *Isulog) Initialize() error {
 		return errors.Wrap(err, "isulog /initialize failed")
 	}
 	defer res.Body.Close()
-	if err = io.Copy(res.Body, ioutil.Discard); err != nil {
+	if _, err = io.Copy(ioutil.Discard, res.Body); err != nil {
 		return errors.Wrap(err, "isulog /initialize failed. body IO")
 	}
 	if res.StatusCode != 200 {
@@ -149,6 +246,9 @@ func (b *Isulog) getLogs(v url.Values) ([]*Log, error) {
 	if err = json.NewDecoder(res.Body).Decode(&r); err != nil {
 		return nil, errors.Wrap(err, "isulog GET /logs decode json failed")
 	}
+	if err = fetchLogDetails(r); err != nil {
+		return nil, err
+	}
 	return r, nil
 }
 
@@ -160,59 +260,80 @@ func fetchLogDetails(logs []*Log) error {
 			if err := json.Unmarshal(l.Data, l.Signup); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
 			}
-			if l.Signup.Name == "" {
-				return errors.Wrapf(err, "[%s] name is empty.", l.Tag)
-			}
-			if l.Signup.BankID == "" {
-				return errors.Wrapf(err, "[%s] bank_id is empty.", l.Tag)
-			}
-			if l.Signup.UserID < 1 {
-				return errors.Wrapf(err, "[%s] user_id is must be upper than 1.", l.Tag)
+			if err := l.Signup.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
 			}
 		case TagSignin:
 			l.Signin = &Signin{}
 			if err := json.Unmarshal(l.Data, l.Signin); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
 			}
+			if err := l.Signin.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
+			}
 		case TagBuyOrder:
 			l.BuyOrder = &Order{}
 			if err := json.Unmarshal(l.Data, l.BuyOrder); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
+			}
+			if err := l.BuyOrder.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
 			}
 		case TagSellOrder:
 			l.SellOrder = &Order{}
 			if err := json.Unmarshal(l.Data, l.SellOrder); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
 			}
+			if err := l.SellOrder.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
+			}
 		case TagBuyError:
 			l.BuyError = &BuyError{}
 			if err := json.Unmarshal(l.Data, l.BuyError); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
+			}
+			if err := l.BuyError.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
 			}
 		case TagBuyDelete:
 			l.BuyDelete = &OrderDelete{}
 			if err := json.Unmarshal(l.Data, l.BuyDelete); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
 			}
+			if err := l.BuyDelete.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
+			}
 		case TagSellDelete:
 			l.SellDelete = &OrderDelete{}
 			if err := json.Unmarshal(l.Data, l.SellDelete); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
+			}
+			if err := l.SellDelete.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
 			}
 		case TagTrade:
 			l.Trade = &Trade{}
 			if err := json.Unmarshal(l.Data, l.Trade); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
 			}
+			if err := l.Trade.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
+			}
 		case TagBuyTrade:
 			l.BuyTrade = &OrderTrade{}
 			if err := json.Unmarshal(l.Data, l.BuyTrade); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
 			}
+			if err := l.BuyTrade.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
+			}
 		case TagSellTrade:
 			l.SellTrade = &OrderTrade{}
 			if err := json.Unmarshal(l.Data, l.SellTrade); err != nil {
 				return errors.Wrapf(err, "[%s] parse failed.", l.Tag)
+			}
+			if err := l.SellTrade.Validate(); err != nil {
+				return errors.Wrapf(err, "[%s] validation failed.", l.Tag)
 			}
 		default:
 			return errors.Errorf("Unknown tag [%s]", l.Tag)
