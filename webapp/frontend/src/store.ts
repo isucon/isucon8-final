c0cb9e414324962c64d17f56f33b1a2b570ec55e
mp3 <im.in.a.coma.xxx@gmail.com>
2018-10-01 14:38:35 +0900

    added modal state, actions, mutations

--- webapp/frontend/src/store.ts
+++ webapp/frontend/src/store.ts
@@ -5,12 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    isModalOpen: false,
+    modalType: 'signup',
   },
   mutations: {
-
+    openModal(state) {
+      state.isModalOpen = true
+    },
+    closeModal(state) {
+      state.isModalOpen = false
+    },
+    setModalType(state, type) {
+      state.modalType = type
+    },
   },
   actions: {
-
+    openSignupModal({ commit }) {
+      commit('setModalType', 'signup')
+      commit('openModal')
+    },
+    openSigninModal({ commit }) {
+      commit('setModalType', 'signin')
+      commit('openModal')
+    },
   },
 })
