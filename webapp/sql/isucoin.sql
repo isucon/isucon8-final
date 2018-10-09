decbc9682f2ce37e7e88787dc2b646ceac497bc7
Kensaku Araga <ken39arg@gmail.com>
2018-10-09 16:10:16 +0900

    user_idにindexを貼って安定させる

--- webapp/sql/isucoin.sql
+++ webapp/sql/isucoin.sql
@@ -26,6 +26,7 @@ CREATE TABLE orders (
     trade_id BIGINT,
     created_at DATETIME(6) NOT NULL,
     INDEX type_closed_at_idx(type, closed_at),
+    INDEX user_id_idx(user_id),
     PRIMARY KEY (id, created_at)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
